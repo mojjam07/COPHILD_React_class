03e8c15adc95e1b679657597b6d08563faa19075
Jamiu Mojeed <mojjam07@gmail.com>
2024-07-17 13:27:17 +0100

    my second commit

---
+++ first_app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Welcome () {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <h2>Welcome to COPHILD REACT CLASS by Mr. Muhyideen</h2>
+      <ul>
+        <li>Great Developer</li>
+        <li>Make money through coding</li>
+        <li>Be a good instructor</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Welcome;
